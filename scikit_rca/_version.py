577a4a5b0847c21c33d3f916250be82b3bb847f5
maxweissenbacher <max.weissenbacher@proton.me>
2026-01-25 15:09:35 +0000

    version bump

--- scikit_rca/_version.py
+++ scikit_rca/_version.py
@@ -28,7 +28,7 @@ version_tuple: VERSION_TUPLE
 commit_id: COMMIT_ID
 __commit_id__: COMMIT_ID
 
-__version__ = version = '0.0.1.post1.dev2'
-__version_tuple__ = version_tuple = (0, 0, 1, 'post1', 'dev2')
+__version__ = version = '0.0.2'
+__version_tuple__ = version_tuple = (0, 0, 2)
 
-__commit_id__ = commit_id = 'g6c3173e4b'
+__commit_id__ = commit_id = None
